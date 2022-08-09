14f910afe6aa3319fb19fd5738bd0902ee6f3868
Julius-Smith <81689210+Julius-Smith@users.noreply.github.com>
2022-08-09 22:05:18 +0200

    .

--- SUPG Julius/example_run.py
+++ SUPG Julius/example_run.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
 
 
 
-    with open('SUPG_xor_cppn9.pkl', 'wb') as output:
+    with open('SUPG_xor_cppn10.pkl', 'wb') as output:
         pickle.dump(winner_net, output, pickle.HIGHEST_PROTOCOL)
         
     #visualize.draw_net(config, winner_net, filename="SUPG_xor_cppn_attemp1")
